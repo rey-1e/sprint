d6a9acc9709525d02dcf2845df23979566e40141
rey-1e <kanaderaj1216@gmail.com>
2026-04-29 18:39:38 +0530

    changed UI for extension's popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,20 @@
 /* popup.js */
-document.querySelector('.btn-donate').addEventListener('click', function (e) {
-  e.preventDefault();
-  chrome.tabs.create({ url: this.href });
+
+function setupLink(elementId) {
+  const el = document.getElementById(elementId);
+  if (el) {
+    el.addEventListener('click', function (e) {
+      e.preventDefault();
+      // Uses the Chrome API to open a new tab securely
+      chrome.tabs.create({ url: this.href });
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Support Donate Link
+  setupLink('donate-link');
+  
+  // getsprint.me Website Link
+  setupLink('website-link');
 });
